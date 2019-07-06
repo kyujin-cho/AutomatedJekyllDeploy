ef0f64062f9790717a4514f5d505afeadd14c557
Kyujin Cho <thy2134@gmail.com>
2019-07-06 15:44:05 +0900

    Initial Commit

---
+++ build.py
@@ -0,0 +1,71 @@
+import subprocess
+import click
+import os 
+import typing 
+from colorama import init, Fore
+from git import Repo
+
+class BuildError(Exception):
+  def __init__(self, message):
+    super().__init__(message)
+    self.message = message
+
+def exec(command: str, cwd=None, debug=False):
+  if debug:
+    print(f'Executing command {command}')
+  print(Fore.YELLOW, end='')
+  proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
+  (out, err) = proc.communicate()
+  if debug:
+    print(out.decode('utf-8').strip())
+
+  print(Fore.BLUE, end='')
+  if len(err) > 0:
+    raise BuildError(err.decode('utf-8').strip())
+  
+@click.group()
+def cli():
+  pass
+
+@cli.command()
+@click.option('--branch', '-b', default='gh-pages', help='Branch name where Github Pages provided')
+@click.option('--backup-branch', default='master', help='Branch name for backup repo')
+@click.option('--debug', '-d', is_flag=True)
+@click.argument('message')
+def build(branch: str, message: str, backup_branch: str, debug: bool):
+  init()
+  cwd = os.getcwd()
+  build_repo = Repo(os.path.join(cwd, '_site'))
+  backup_repo = Repo(cwd)
+
+  print(Fore.BLUE, end='')
+  
+  print('Building site for production ready')
+  exec('bundle exec jekyll build', debug=debug)
+
+  print('Adding new files on Github Pages git folder')
+  index = build_repo.index
+  index.add(['*'])
+
+  print('Making commit for changes on website')
+  index.commit(message)
+
+  print('Pushing changes to Github Pages repository')
+  build_repo.remote(name='origin').push(refspec=f'refs/heads/{branch}:refs/heads/{branch}')
+  
+  print('Adding new files on backup git')
+  exec('git add *')
+  # backup_index.add(['*'])
+
+  print('Making commit for change on backup')
+  backup_index = backup_repo.index
+  backup_index.commit(message)
+
+  print('Pushing changes to backup repository')
+  backup_repo.remote(name='origin').push(refspec=f'refs/heads/{backup_branch}:refs/heads/{backup_branch}')
+
+  print(Fore.GREEN + 'Done!')
+  print(Fore.RESET)
+
+if __name__ == '__main__':
+    cli()
---
+++ healthcheck.py
@@ -0,0 +1,43 @@
+import os
+import requests
+import CloudFlare
+
+BLOG_DOMAIN=os.environ['BLOG_DOMAIN']
+SERVER_ADDR=os.environ['BLOG_SERVER_ADDR']
+FALLBACK_ADDR=os.environ['BLOG_FALLBACK_ADDR']
+CF_API_KEY=os.environ['CF_API_KEY']
+CF_API_EMAIL=os.environ['CF_API_EMAIL']
+CF_ZONE_ID=os.environ['CF_ZONE_ID']
+
+mode = 'default'
+
+def health_check() -> bool:
+  try:
+    requests.get(SERVER_ADDR)
+    return True
+  except:
+    return False
+
+def get_record_id(cf: CloudFlare.CloudFlare) -> str:
+  records = cf.zones.dns_records.get(CF_ZONE_ID, params={
+    'name': BLOG_DOMAIN,
+    'type': 'CNAME'
+  })
+  return records.result[0].id
+
+def switch(mode: str):  
+  cf = CloudFlare.CloudFlare()
+  response = cf.zones.dns_records.put(CF_ZONE_ID, get_record_id(cf), data={
+    'type': 'CNAME', 
+    'name': BLOG_DOMAIN,
+    'content': SERVER_ADDR if mode == 'fallback' else FALLBACK_ADDR,
+    'proxied': False
+  })
+  return response
+
+def main():
+  healthy = health_check()
+  if healthy and mode == 'fallback':
+    switch('default')
+  elif not healthy and mode == 'default':
+    switch('fallback')
